e9c44b291800d70d16c4f67b4be37f13c26868bd
tigertina <tigeryjchen@gmail.com>
2015-12-22 17:09:41 +0800

    leetcode 125. Valid Palindrome
    Valid Palindrome
    使用正则预处理

---
+++ Solution125.java
@@ -0,0 +1,32 @@
+/**
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+ * }
+ */
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length()==0)
+    		return true;
+    	s = s.replaceAll("[^a-zA-Z0-9]", "");
+    	s = s.toLowerCase();
+    	int i = 0;
+    	int j = s.length()-1;
+    	while(i<j){
+    		if(s.charAt(i)==s.charAt(j)){
+    			i++;
+    			j--;
+    		}else{
+    			break;
+    		}    		
+    	}
+    	return i>=j;
+    }
+}
