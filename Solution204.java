addd2d06ebbd029d425539544f256d1a819daa08
tigertina <tigeryjchen@gmail.com>
2015-12-22 17:12:04 +0800

    leetcode 204. Count Primes
    Count Primes 完全是按照提示做的，精华在
    Sieve of Eratosthenes

---
+++ Solution204.java
@@ -0,0 +1,27 @@
+/**
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+
+ */
+public class Solution {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+          isPrime[i] = true;
+        }
+        // Loop's ending condition is i * i < n instead of i < sqrt(n)
+        // to avoid repeatedly calling an expensive function sqrt().
+        for (int i = 2; i * i < n; i++) {
+          if (!isPrime[i]) continue;
+          for (int j = i * i; j < n; j += i) {
+             isPrime[j] = false;
+          }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+          if (isPrime[i]) count++;
+        }
+        return count;
+    }
+}
