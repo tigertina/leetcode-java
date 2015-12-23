3d747f4d7475a3882070428e6fd5ecd27bc3a7d8
tigertina <tigeryjchen@gmail.com>
2015-12-23 15:17:12 +0800

    leetcode 206 Reverse a singly linked list
    递归和非递归实现

---
+++ Solution206_1.java
@@ -0,0 +1,23 @@
+/**
+Reverse a singly linked list.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+		while(head!=null){
+			ListNode tmp = head.next;
+			head.next = prev;
+			prev = head;
+			head = tmp;
+		}
+		return prev;
+    }
+}
